dad97bb9c7e8dbf29f919f1acfb274d1b39aa856
MattMoe <mrobertmoe@gmail.com>
2023-05-18 19:04:14 -0400

    draw player outline

--- js/app.js
+++ js/app.js
@@ -2,3 +2,22 @@ const canvas = document.querySelector('#spacer');
 const ctx = canvas.getContext('2d');
 const { width, height } = canvas;
 
+const player = {
+  x: 400,
+  y: 300,
+  color: "white",
+  draw() {
+    ctx.strokeStyle = this.color;
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x - 10, this.y + 20);
+    ctx.lineTo(this.x + 10, this.y + 20);
+    ctx.lineTo(this.x, this.y);
+    
+    // triangle outline
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 1;
+    ctx.stroke();
+  }
+}
+player.draw();
